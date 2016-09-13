209a0082c69124cbd226b8429adc071828d3b471
Marcel <marcelbm94@gmail.com>
2016-09-13 07:56:46 -0500

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Role.create([{ name: 'admin' }, { name: 'regular_user' }])
-admin = User.create(email: 'admin', name: 'admin',
+admin = User.create(email: 'admin@admin.com', name: 'admin',
                     last_name: 'admin', username: 'admin', password: 'aguacate',
                     password_confirmation: 'aguacate', role_id: 1)
